28bea9a2fb5e6ee3efafea6443ef49cb3bc1db2e
Adil Shaikh <89292349+adilshaikh19@users.noreply.github.com>
2023-08-23 10:11:19 +0530

    Update settings.py

--- shoppingly/settings.py
+++ shoppingly/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&6m&1pdx#4a4dt%6lw6mcv!p$q3-32$p(dptdgoef_ytg6p*ht'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
